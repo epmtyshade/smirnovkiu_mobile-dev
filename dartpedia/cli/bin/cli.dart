669285eae87ace5fd0ab98540be7c7419323d838
Kirill Smirnov <KirYrSm@yandex.ru>
2026-04-03 22:53:45 +0300

    Добавлена константа version и обработка аргумента version

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,6 +1,13 @@
-// Импорт не используется, так как мы не вызываем функций из пакета cli
+// Объявление константы для версии приложения
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  // Вывод приветственного сообщения в консоль
-  print('Hello, Kirill!');
+  // Если аргументы командной строки не переданы - выводим приветствие
+  if (arguments.isEmpty) {
+    print('Hello, Kirill!');
+  } 
+  // Если первый аргумент равен 'version' - выводим версию CLI
+  else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version'); // $version - строковая интерполяция
+  }
 }
